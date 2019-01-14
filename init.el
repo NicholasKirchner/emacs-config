78ef236a8196c3ebf1d6d5c8e47c8fcc6c984a17
Nicholas Kirchner <nicholas.r.kirchner.1@gmail.com>
2019-01-14 08:04:29 -0600

    Initial commit -- ui is OK.

---
+++ init.el
@@ -0,0 +1,25 @@
+(defvar current-user
+  (getenv
+    (if (equal system-type 'windows-nt) "USERNAME" "USER")))
+
+(defvar my-emacs-dir (file-name-directory load-file-name))
+(defvar my-modules-dir (expand-file-name "modules" my-emacs-dir))
+(setq custom-file (expand-file-name "custom.el" my-emacs-dir))
+
+(add-to-list 'load-path my-modules-dir)
+
+(require 'my-packages)
+
+;;ui stuff
+
+(load-theme 'sanityinc-tomorrow-night t)
+
+(menu-bar-mode (if (eq system-type 'darwin) 1 -1))
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+
+(blink-cursor-mode -1)
+
+(setq inhibit-startup-screen t)
+
+(set-default-font "Inconsolata-dz 10")
---
+++ modules/my-packages.el
@@ -0,0 +1,25 @@
+(require 'package)
+(require 'cl) ;; For every function
+
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
+(setq package-user-dir (expand-file-name "elpa" my-emacs-dir))
+(package-initialize)
+
+(defvar my-theme-packages
+  '(zenburn-theme
+    solarized-theme
+    color-theme-sanityinc-tomorrow))
+
+(defun install-packages (list-of-packages)
+  (unless (every #'package-installed-p list-of-packages)
+    (package-refresh-contents)
+    (mapc (lambda (package)
+                  (unless (package-installed-p package)
+                          (package-install package)))
+            list-of-packages)))
+
+(install-packages my-theme-packages)
+
+(provide 'my-packages)
